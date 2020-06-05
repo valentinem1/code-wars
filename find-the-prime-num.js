7cd50926cfe0bbdc7139b4d12af2cca50247a41d
Valentine <valentinemaillard1@gmail.com>
2020-06-05 16:00:51 -0400

    find prime numbers in an array solution

---
+++ find-the-prime-num.js
@@ -0,0 +1,23 @@
+// given an array of integer return only the prime numbers
+
+function isPrime(arr){
+    // assuming all numbers are whole and positive numbers
+    // create a new array to hold the prime numbers
+    // loops through the array of numbers/
+    // first check if the number is bigger than 1 as one isn't a prime number
+    // check if the number equals 2 as 2 is the only prime number divisible by 2
+    // and check if the number divided by 2 is a float number
+    // non prime numbers while return a whole number when divided by 2
+    // if the number passes these checks it is a prime number and push it to the new array!
+    let newArr = [];
+    
+    for(let num of arr){
+        if(num > 1){
+            if(num === 2 || (num / 2) % 1 !== 0){
+            newArr.push(num);
+            };
+        };
+    }
+    return newArr;
+}
+console.log(isPrime([1, 2, 3, 4, 5, 6, 7, 11, 76, 48]));
