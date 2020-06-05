054938b79cbfa03da04600fc64374912ec9e762e
Valentine <valentinemaillard1@gmail.com>
2020-06-05 16:06:47 -0400

    find prime num in array solution with filter method

--- find-the-prime-num.js
+++ find-the-prime-num.js
@@ -9,15 +9,25 @@ function isPrime(arr){
     // and check if the number divided by 2 is a float number
     // non prime numbers while return a whole number when divided by 2
     // if the number passes these checks it is a prime number and push it to the new array!
-    let newArr = [];
+    // let newArr = [];
     
-    for(let num of arr){
+    // for(let num of arr){
+    //     if(num > 1){
+    //         if(num === 2 || (num / 2) % 1 !== 0){
+    //         newArr.push(num);
+    //         };
+    //     };
+    // }
+    // return newArr;
+
+    // the filter method will return the same array with only the prime members
+    // it avoids creating a new array
+    return arr.filter(num => {
         if(num > 1){
-            if(num === 2 || (num / 2) % 1 !== 0){
-            newArr.push(num);
-            };
-        };
-    }
-    return newArr;
+            if(num === 2 || (num / 2) % 1 !==0){
+                return num;
+            }
+        }
+    })
 }
 console.log(isPrime([1, 2, 3, 4, 5, 6, 7, 11, 76, 48]));
